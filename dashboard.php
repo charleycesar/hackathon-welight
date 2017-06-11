f55ac52373127979a8c405b71824481a1d0e862d
MASTER\Daniel <contato.fabbri@gmail.com>
2017-06-10 22:38:36 -0300

    Base

---
+++ dashboard.php
@@ -0,0 +1,9 @@
+<?php inclulde "header.php"; ?>
+
+<div class="row">
+<div class="container">
+
+</div>
+</div>
+
+<?php inclulde "footer.php"; ?>
---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+      <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <!--Import materialize.css-->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<title></title>
+</head>
+<body>
+
+<nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo left">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+      </ul>
+      <a href="causa.html" class="right"><i class="material-icons">add</i></a>
+    </div>
+
+   
+  </nav>
